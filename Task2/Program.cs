fe8e9e90bc9cf9330c634a1f7fe0a3b1959897ef
Gorik <gosha_qwerty@rambler.ru>
2022-11-30 00:00:58 +0800

    Task 2

---
+++ Task2/Program.cs
@@ -0,0 +1,21 @@
+﻿ // Task 2
+ 
+ Console.Clear();
+Console.Write("Введите первое число");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число");
+int m = Convert.ToInt32(Console.ReadLine());
+
+if (n > m)
+{
+Console.WriteLine (" {0} max", n);
+Console.WriteLine (" {0} min", m);
+}
+
+
+else 
+{
+Console.WriteLine (" {0} max", m);
+Console.WriteLine (" {0} min", n);
+}
+    
